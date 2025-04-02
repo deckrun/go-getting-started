b4ce49147105385d598e8d3a74853eb6fd08e4f6
Daniel Vigueras <daniel@vigueras.dev>
2025-04-02 08:33:18 +0200

    Add logging middleware to HTTP handlers for request logging

--- main.go
+++ main.go
@@ -4,18 +4,37 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.Handle("/", loggingMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, "Hello World")
-	})
+	})))
 
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	http.Handle("/healthz", loggingMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, "ok")
-	})
+	})))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Printf("%s - - [%s] \"%s %s %s\" %d %d \"%s\" \"%s\"",
+			r.RemoteAddr,
+			start.Format("02/Jan/2006:15:04:05 -0700"),
+			r.Method,
+			r.RequestURI,
+			r.Proto,
+			http.StatusOK,
+			r.ContentLength,
+			r.Referer(),
+			r.UserAgent(),
+		)
+	})
 }
